fe7e7d63a2670f03103234637db6eaa151bf97f3
andrew sincere <aws1132@g.rit.edu>
2021-12-06 23:29:17 -0500

    day 6 puzz 1 and 2

--- day5puzz1.py
+++ day5puzz1.py
@@ -10,7 +10,6 @@ for line in data:
             x1,x2 = x2,x1
         if y1>y2:
             y1,y2 = y2,y1
-        print(x1,y1," ",x2,y2)
         for x in range(x1,x2+1):
             for y in range(y1,y2+1):
                 print(x,y)
