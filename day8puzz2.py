dd98dc736a5bd7a3de3bd7c114f0f3053069c3d2
andrew sincere <aws1132@g.rit.edu>
2021-12-08 05:21:38 -0500

    faster version of my day 8 part 2

--- day8puzz2.py
+++ day8puzz2.py
@@ -1,44 +1,11 @@
 from itertools import permutations
-import time
-
-start = time.perf_counter()
-
-numbers = [list('123567'),list('36'),list('13457'),list('13467'),list('2346'),list('12467'),list('124567'),list('136'),list('1234567'),list('123467')]
-
-with open('input.txt') as file:
-    data = [[el[0].strip().split(),el[1].strip().split()] for el in [line.strip().split('|') for line in file]]
-
+numbers = ['123567','36','13457','13467','2346','12467','124567','136','1234567','123467']
+with open('input.txt') as file: data = [[el[0].strip().split(),el[1].strip().split()] for el in [line.strip().split('|') for line in file]]
 digitlist = []
 for el in data:
-    mapset = [set(number) for number in el[0]] #a list of sets where each set is a signal sent
     for wiremap in permutations(list('abcdefg')):
-        reslist = []
-        for number in numbers:
-            currentnum = ''
-            for segments in number:
-                currentnum += wiremap[int(segments)-1]
-            reslist.append(currentnum)
-        resset = [set(number) for number in reslist]
-        count = ''
-        for number in resset:
-            flag = True
-            for number2 in mapset:
-                if flag:
-                    if (number==number2):
-                        count += str(resset.index(number))
-                        flag = False
-
-        if len(count) == 10:
-            fourdigit = ''
-            digitdisplay = [set(number) for number in el[1]]
-            for digit in digitdisplay:
-                fourdigit+=str((resset.index(digit)))
-            digitlist.append(int(fourdigit))
+        resset = ([set(number) for number in [[wiremap[int(segments)-1] for segments in number] for number in numbers]])
+        if len([" " for number2 in [set(number) for number in el[0]] for number in resset if number==number2])==10:
+            digitlist.append(int(str([str((resset.index(digit)))for digit in [set(number) for number in el[1]]]).replace("', '",'')[2:6]))
             break
-    print(digitlist)
-
 print(sum(digitlist))
-
-stop = time.perf_counter()
-
-print(stop-start)
